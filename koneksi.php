37503f620fbd61df4e06c4d3e49960948f2154ca
Fadel Maulana <me.fadelmaulana@gmail.com>
2022-08-22 18:53:29 +0700

    update sql

--- koneksi.php
+++ koneksi.php
@@ -2,7 +2,7 @@
 $host = "localhost";
 $user = "root";
 $pass = "";
-$name = "dish4254_simtrans";
+$name = "simtrans";
 
 $koneksi = mysqli_connect($host, $user, $pass, $name) or die("Koneksi ke database gagal!");
 ?>
